3ca2cc17d2221171a23c594a47970d8c1f6794ac
Varthya398 <84780022+Varthya398@users.noreply.github.com>
2021-06-04 13:24:50 +0530

    Wallis. C

--- wallis.c
+++ wallis.c
@@ -32,3 +32,11 @@ float wallis_pi(int count){
     }
     return pi * 2;
 }
+float wallis_pi(int count){
+    float pi = 1.0;
+    for (int i = 1; i < count; i++){
+        float num = 4.0 * i * i;
+        pi *= num / (num - 1);
+    }
+    return pi * 2;
+}
